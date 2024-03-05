300a63eb9b0a0d48e6736c283c2547f847ab4336
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-03-05 21:17:12 +0900

    Title: 가장 많은 데이터, Time: 197ms, Memory: 9MB, Status: Passed - CodeTree

--- 240305/가장 많은 데이터/most-frequent-data.cpp
+++ 240305/가장 많은 데이터/most-frequent-data.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-// #include <vector>
 #include <unordered_map>
 #include <algorithm>
 
@@ -11,29 +10,19 @@ int main() {
     cin >> n;
 
     unordered_map<string, int> um;
-    // vector<string> v;
     int answer = 1;
     for(int i = 0; i < n; i++) {
         string color;
         cin >> color;
 
         if(um.find(color) != um.end()) {
-            // um[color]++;
             answer = max(answer, ++um[color]);
         }
         else {
             um[color] = 1;
-            // v.push_back(color);
         }
     }
-
-    // int answer = 0, len = v.size();
-    // for(int i = 0; i < len; i++) {
-    //     if(answer < um[v[i]]) {
-    //         answer = um[v[i]];
-    //     }
-    // }
-
+    
     cout << answer;
 
     return 0;
