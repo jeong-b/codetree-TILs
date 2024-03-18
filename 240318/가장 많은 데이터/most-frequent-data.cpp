5e54d2c62392fa23a7fc138942978f2148035f1f
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-03-18 10:29:31 +0900

    Title: 가장 많은 데이터, Time: 232ms, Memory: 9MB, Status: Passed - CodeTree

--- 240318/가장 많은 데이터/most-frequent-data.cpp
+++ 240318/가장 많은 데이터/most-frequent-data.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,17 +9,23 @@ int main() {
 	/* 변수 선언 */
 	int n; // 입	력할 문자열의 개수 int n
 	string color; // 입력할 문자열 string color
-	map<string, int> m; // 문자열을 저장하는 map<string, int> m
+	unordered_map<string, int> um; // 문자열을 저장하는 unordered_map<string, int> um
+	int answer = 0; // 최대 횟수를 저장하는 int answer
 
 	/* 변수 입력 */
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> color;
-		if (m.find(color) == m.end()) m.insert({ color, 1 });
-		else m[color]++;
+
+		if (um.find(color) == um.end()) um.insert({ color, 1 });
+		else um[color]++;
+
+		answer = max(answer, um[color]);
 	}
-	cout << m.begin()->second;
+
+	/* 정답 출력 */
+	cout << answer;
 
 	return 0;
 }
