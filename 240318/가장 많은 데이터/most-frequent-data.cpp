b6a95722ed24559fe575e4b6f84ebb9403de5372
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-03-18 10:31:07 +0900

    Title: 가장 많은 데이터, Time: 250ms, Memory: 19MB, Status: Passed - CodeTree

--- 240318/가장 많은 데이터/most-frequent-data.cpp
+++ 240318/가장 많은 데이터/most-frequent-data.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
-#include <string>
 #include <unordered_map>
+#include <string>
 #include <algorithm>
+
+#define MAX_N 100000
 
 using namespace std;
 
+// 변수 선언
+int n;
+string words[MAX_N];
+unordered_map<string, int> freq;
+
+int ans; // 가장 많이 나온 횟수를 기록합니다.
+
 int main() {
-	/* 변수 선언 */
-	int n; // 입	력할 문자열의 개수 int n
-	string color; // 입력할 문자열 string color
-	unordered_map<string, int> um; // 문자열을 저장하는 unordered_map<string, int> um
-	int answer = 1; // 최대 횟수를 저장하는 int answer
+    // 입력:
+    cin >> n;
 
-	/* 변수 입력 */
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> color;
+    for(int i = 0; i < n; i++)
+        cin >> words[i];
+    
+    // 각 문자열이 몇 번씩 나왔는지를
+    // hashmap에 기록해줍니다.
+    for(int i = 0; i < n; i++) {
+        // 처음 나온 문자열이라면 1을 직접 적어줘야 합니다.
+        if(freq.find(words[i]) == freq.end())
+            freq[words[i]] = 1;
+        // 이미 나와있던 문자열이라면 1을 더해줍니다.
+        else
+            freq[words[i]]++;
+        
+        // 최댓값을 갱신합니다.
+        ans = max(ans, freq[words[i]]);
+    }
 
-		if (um.find(color) == um.end()) um.insert({ color, 1 });
-		else answer = max(answer, ++um[color]);
-	}
-
-	/* 정답 출력 */
-	cout << answer;
-
-	return 0;
+    cout << ans;
+    return 0;
 }
