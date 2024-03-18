771be2cdd3308e6e54daccd247ba6e4ec951139f
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-03-18 10:16:09 +0900

    Title: 숫자 등장 횟수, Time: 146ms, Memory: 1MB, Status: Passed - CodeTree

--- 240318/숫자 등장 횟수/number-frequency.cpp
+++ 240318/숫자 등장 횟수/number-frequency.cpp
@@ -1,45 +1,29 @@
 #include <iostream>
 #include <unordered_map>
 
-#define MAX_N 100000
-
 using namespace std;
 
-// 변수 선언
-int n, m;
-int arr[MAX_N];
-unordered_map<int, int> freq;
-
 int main() {
-    // 입력:
-    cin >> n >> m;
+	/* 변수 선언 */
+	int n, m; //  원소의 개수  int n, 질의의 개수 int m;
+	unordered_map<int, int> um; // 값을 저장하는 unordered_map<int, int> um
+	int input; // 입력으로 주어지는 값  int input
 
-    for(int i = 0; i < n; i++)
-        cin >> arr[i];
-    
-    // 각 숫자가 몇 번씩 나왔는지를
-    // hashmap에 기록해줍니다.
-    for(int i = 0; i < n; i++) {
-        // 처음 나온 숫자라면 1을 직접 적어줘야 합니다.
-        if(freq.find(arr[i]) == freq.end())
-            freq[arr[i]] = 1;
-        // 이미 나와있던 숫자라면 1을 더해줍니다.
-        else
-            freq[arr[i]]++;
-    }
+	/* 변수 입력 */
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
 
-    // m개의 질의에 대해
-    // 몇 번씩 나왔는지를 출력합니다.
-    for(int i = 0; i < m; i++) {
-        int num;
-        cin >> num;
+		if (um.find(input) == um.end()) um.insert({ input, 1 });
+		else um[input]++;
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cin >> input;
+		if (um.find(input) == um.end()) cout << 0 << ' ';
+		else cout << um[input] << ' ';
+	}
 
-        // 처음 나온 숫자라면 0을 출력합니다.
-        if(freq.find(num) == freq.end())
-            cout << 0 << " ";
-        // 나온 적이 있는 숫자라면, 빈도수를 출력해줍니다.
-        else
-            cout << freq[num] << " ";
-    }
-    return 0;
+	return 0;
 }
