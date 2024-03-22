1b26ca31e49190e15805de5db69c6a323abb5224
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-03-22 20:32:26 +0900

    Title: 정수 n개의 합 2, Time: 31ms, Memory: 1MB, Status: Passed - CodeTree

--- 240322/정수 n개의 합 2/sum-of-n-integers-2.cpp
+++ 240322/정수 n개의 합 2/sum-of-n-integers-2.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <climits>
+
+#define MAX_N 100000
 
 using namespace std;
 
+// 변수 선언
+int n, k;
+int arr[MAX_N + 1];
+int prefix_sum[MAX_N + 1];
+int ans = INT_MIN;
+
+// [s, e] 구간 내의 원소의 합을 반환합니다.
+int GetSum(int s, int e) {
+    return prefix_sum[e] - prefix_sum[s - 1];
+}
+
 int main() {
-	/* 변수 선언 */
-	int n, k; // 원소의 개수 int n, 합을 구하기 위한 원소의 수 int k;
-	int input; // 입력 받는 원소 int input
-	int answer; // 최댓값을 저장하는 int answer;
-	vector<int> num_list, prefix_sum; // 원소를 저장하는  vector<int> num_list, 누적합을 저장하는 vector<int> prefix_sum 
-	
-	/* 변수 입력 */
-	cin >> n >> k;
+    // 입력:
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++)
+        cin >> arr[i];
 
-	cin >> input;
-	num_list.push_back(input);
-	prefix_sum.push_back(input);
+    // 누적합 배열을 만들어줍니다.
+    prefix_sum[0] = 0;
+    for(int i = 1; i<= n; i++)
+        prefix_sum[i] = prefix_sum[i - 1] + arr[i];
+    
+    // 모든 구간에 대해 합을 찾아
+    // 그 중 최댓값을 갱신합니다.
+    for(int i = 1; i <= n - k + 1; i++)
+        ans = max(ans, GetSum(i, i + k - 1));
 
-	for (int i = 1; i < n; i++)
-	{
-		cin >> input;
-		num_list.push_back(input);
-		prefix_sum.push_back(input + prefix_sum[i - 1]);		
-	}
-
-	/* 풀이 */
-	answer = prefix_sum[k - 1];
-	for (int i = 0; i < n - k; i++) answer = max(answer, prefix_sum[i + k] - prefix_sum[i]);
-	
-	/* 출력 */
-	cout << answer;
-
-	return 0;
+    cout << ans;
+    return 0;
 }
