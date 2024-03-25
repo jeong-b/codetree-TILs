9a85b46c2c56132b2252cea4e904da4ce81c9bd6
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-03-25 11:35:14 +0900

    Title: 정수 세 개 입력받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240325/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
+++ 240325/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -3,10 +3,8 @@
 using namespace std;
 
 int main() {
-    double a, b, c;
+    int a, b, c;
     cin >> a >> b >> c;
-    cout << fixed;
-    cout.precision(3);
     cout << a << ' ' << b << ' ' << c;
 	return 0;
 }
