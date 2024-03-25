aeb8d79f8eee45d3b154183c7ffff4eedd0157f5
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-03-25 11:38:18 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240325/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a, b;
+    char c;
+    cin >> c >> a >> b;
+    cout << c << endl;
+    cout << fixed;
+    cout.precision(2);
+    cout << a << endl << b;
+	return 0;
+}
