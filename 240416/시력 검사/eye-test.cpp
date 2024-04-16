7bd1773a5946a641efeb8987720dd048003bc8e1
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-04-16 22:45:53 +0900

    Title: 시력 검사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240416/시력 검사/eye-test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float l, r;
+    cin >> l >> r;
+    if (l >= 1.0 && r >= 1.0) cout << "High";
+    else if (l >= 0.5 && r >= 0.5) cout << "Middle";
+    else cout << "Low";
+	return 0;
+}
