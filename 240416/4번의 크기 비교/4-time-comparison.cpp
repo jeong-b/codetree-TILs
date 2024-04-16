031133e487461bc8b3aee9e6167e880418c7ee71
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-04-16 22:41:14 +0900

    Title: 4번의 크기 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240416/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    cout << (a > b) << endl << (a > c) << endl << (a > d) << endl << (a > e);
+	return 0;
+}
