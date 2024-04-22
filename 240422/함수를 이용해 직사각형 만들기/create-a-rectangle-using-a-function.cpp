6f37968af74b2ac7e7e254cd23b18d95b49deb77
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-04-22 21:20:59 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void makeRectangle(int n, int m) {
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            cout << 1;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    makeRectangle(n, m);
+	return 0;
+}
