1bbe11bc7c25c0fe99d1ae2416a210d8ddb6e8eb
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-10-01 14:37:18 +0900

    Title: 마을 구분하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241001/마을 구분하기/seperate-village.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define MAX_SIZE 25
+
+using namespace std;
+
+int n;
+bool map[MAX_SIZE][MAX_SIZE];
+bool visited[MAX_SIZE][MAX_SIZE];
+int dir[2][2] = { {0,1}, {1,0} };
+vector<int> town;
+priority_queue<int, vector<int>, greater<int>> answer;
+
+bool InRange(int y, int x)
+{
+	return x >= 0 && x < n && y >= 0 && y < n;
+}
+
+bool CanGo(int y, int x)
+{
+	if (!InRange) return false;
+	if (!map[y][x] || visited[y][x]) return false;
+	return true;
+}
+
+void FindTown(int y, int x, int townNum)
+{
+	for (int i = 0; i < 2; i++)
+	{
+		int nextY = y + dir[i][0];
+		int nextX = x + dir[i][1];
+
+		if (!CanGo(nextY, nextX)) continue;
+		visited[nextY][nextX] = true;
+		town[townNum]++;
+		FindTown(nextY, nextX, townNum);
+	}	
+}
+
+
+int main()
+{
+	// input
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> map[i][j];
+		}
+	}
+	
+	// solve
+	int townNum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (!CanGo(i, j)) continue;
+
+			town.push_back(1);
+			FindTown(i, j, townNum);
+			townNum++;
+		}
+	}
+	for (int i = 0; i < town.size(); i++)
+	{
+		answer.push(town[i]);
+	}
+
+	// output
+	cout << answer.size() << endl;
+	while (!answer.empty())
+	{
+		cout << answer.top() << endl;
+		answer.pop();
+	}
+
+	return 0;
+}
