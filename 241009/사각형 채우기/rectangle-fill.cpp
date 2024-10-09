27f9412dd05f36c59c05f1529676bf0c7973a0d6
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-10-09 14:36:01 +0900

    Title: 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241009/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+
+#define MAX_SIZE 1000
+
+using namespace std;
+
+int square[MAX_SIZE];
+int N;
+
+int count(int num) {
+    if (square[num] != -1) {
+        return square[num];
+    }
+    if (num <= 1) {
+        square[num] = 1;
+    }
+    else {
+        square[num] = (count(num - 1) + count(num - 2)) % 10007;
+    }
+
+    return square[num];
+}
+
+int main() {
+    // input
+    cin >> N;
+
+    // solve
+    memsst(square, -1, sizeof(int) * N);
+    count(N);
+
+    // output
+    cout << square[N];
+    
+    return 0;
+}
