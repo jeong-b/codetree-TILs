b6cf8b8099d5959217176ef79682bbedac8ef8d9
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-10-09 14:22:17 +0900

    Title: 계단 오르기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241009/계단 오르기/climbing-stairs.cpp
+++ 241009/계단 오르기/climbing-stairs.cpp
@@ -5,21 +5,21 @@
 
 using namespace std;
 
-long long routes[MAX_SIZE];
+int routes[MAX_SIZE];
 int N;
 
-long long stair (int floor) {
+int stair (int floor) {
     if (routes[floor] != -1) {
         return routes[floor];
     }
     if (floor <= 1) {
         routes[floor] = 0; 
     }
-    else if (floor <= 3) {
+    else if (floor <= 4) {
         routes[floor] = 1;
     }
     else {
-        routes[floor] = stair(floor - 2) + stair(floor - 3);
+        routes[floor] = (stair(floor - 2) + stair(floor - 3)) % 10007;
     }
 
     return routes[floor];
@@ -31,11 +31,11 @@ int main() {
     cin >> N;
 
     // solve
-    memset(routes, -1, sizeof(long long) * MAX_SIZE);
+    memset(routes, -1, sizeof(int) * MAX_SIZE);
     stair(N);
 
     // output
-    cout << routes[N] % 10007;
+    cout << routes[N];
 
     return 0;
 }
