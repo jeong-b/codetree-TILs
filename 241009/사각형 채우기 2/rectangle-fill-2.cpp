7efd92e8e254d38c3f7ebdc505624626c7dadc25
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-10-09 17:56:50 +0900

    Title: 사각형 채우기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241009/사각형 채우기 2/rectangle-fill-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+
+#define MAX_SIZE 1001
+
+using namespace std;
+
+int dp[MAX_SIZE];
+int N;
+
+int count(int num) {
+    if (dp[num] != -1) { return dp[num]; }
+
+    if (num == 1) { dp[num] = 1; }
+    else if (num == 2) { dp[num] = 3; }
+    else { dp[num] = count(num - 1) + (count(num - 2) * 2); }
+
+    return dp[num];
+}
+
+
+int main() {
+    cin >> N;
+
+    memset(dp, -1, sizeof(int) * MAX_SIZE);
+    count(N);
+
+    cout << dp[N];
+
+    return 0;
+}
