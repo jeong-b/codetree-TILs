66ae3e58d9dccdf096348b760be57883493cfc2a
서정빈 <122597075+jeong-b@users.noreply.github.com>
2024-12-24 10:07:49 +0900

    Title: 최고의 33위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241224/최고의 33위치/best-place-of-33.cpp
+++ 241224/최고의 33위치/best-place-of-33.cpp
@@ -7,40 +7,47 @@ using namespace std;
 int N;
 int map[MAX_SIZE][MAX_SIZE];
 
-// 변수 N을 입력받는 함수
+// 입력 함수
 void input()
 {
-	cin >> N;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			cin >> map[i][j];
-		}
-	}
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin >> map[i][j];
+        }
+    }
 }
 
-// 격자내 완전탐색을 통한 최댓값 찾기
-int find()
+// 3x3 서브그리드 내 합의 최댓값 찾기
+int findMaxSubgridSum()
 {
-	int result = 0; // 계산의 결과 값
-	int temp = 0; // 비교를 위한 변수
-	// 완전탐색하며 최댓값 찾기
-	for (int i = 0; i < N - 2; i++)
-	{
-		for (int j = 0; j < N - 2; j++)
-		{
-			temp = map[i][j] + map[i + 1][j] + map[i + 2][j] + map[i][j + 1] + map[i][j + 2] + map[i + 1][j + 1] + map[i + 1][j + 2] + map[i + 2][j + 1] + map[i + 2][j + 2];
-			result = max(result, temp);
-		}
-	}
-	return result;
+    const int SUBGRID_SIZE = 3;
+    int result = 0; // 최댓값 저장
+    int temp = 0;   // 현재 서브그리드의 합
+
+    // 격자 탐색
+    for (int i = 0; i <= N - SUBGRID_SIZE; i++)
+    {
+        for (int j = 0; j <= N - SUBGRID_SIZE; j++)
+        {
+            // 3x3 서브그리드 합 계산
+            temp = map[i][j] + map[i][j + 1] + map[i][j + 2]
+                + map[i + 1][j] + map[i + 1][j + 1] + map[i + 1][j + 2]
+                + map[i + 2][j] + map[i + 2][j + 1] + map[i + 2][j + 2];
+
+            result = max(result, temp);
+        }
+    }
+    return result;
 }
 
 int main()
 {
-	input();
-	int answer = find(); // 격자내 최댓값을 나타내는 변수
-	cout << answer;
-	return 0;
+    input();
+    int answer = findMaxSubgridSum();
+    cout << answer;
+    return 0;
 }
