ef2e2f678800df30aa0664f71518a9bfb826639b
서정빈 <122597075+jeong-b@users.noreply.github.com>
2025-02-17 22:19:58 +0900

    Title: 자주 등장한 top K 숫자, Time: 52ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 250217/자주 등장한 top K 숫자/top-k-frequent-elements.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+int N, K;
+unordered_map<int, int> um;
+
+int main() {
+    cin >> N >> K;
+
+    for (int i = 0; i < N; ++i) {
+        int number;
+        cin >> number;
+        um[number]++;
+    }
+
+    auto cmp = [](const pair<int, int>& left, const pair<int, int>& right) {
+        if (left.second == right.second) return left.first < right.first;
+        return left.second < right.second;
+    };
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
+
+    for (const auto &item : um) {
+        pq.push(item);
+    }
+
+    for (int i = 0; i < K; i++) {
+        cout << pq.top().first << ' ';
+        pq.pop();
+    }
+
+    return 0;
+}
