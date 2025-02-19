488fdbf14657ef0ab86ea23e67aa81b2cb5f92ec
서정빈 <122597075+jeong-b@users.noreply.github.com>
2025-02-19 21:30:55 +0900

    Title: 자리 바꾸기 2, Time: 42ms, Memory: 8MB, Status: Passed - CodeTree

--- 250219/자리 바꾸기 2/changing-seats-2.cpp
+++ 250219/자리 바꾸기 2/changing-seats-2.cpp
@@ -1,53 +1,51 @@
 #include <iostream>
+#include <vector>
 #include <unordered_set>
+#include <algorithm>
+
 using namespace std;
 
-#define MAX_N 100000
-#define MAX_K 100000
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-// 변수 선언
-int n, k;
-int a[MAX_K], b[MAX_K];
-int arr[MAX_N + 1];
-int ans[MAX_N + 1];
-unordered_set<int> s[MAX_N + 1];
+    int N, K;
+    cin >> N >> K;
 
-int main(){
-    // 입력:
-    cin >> n >> k;
-    for(int i = 0; i < k; i++)
-        cin >> a[i] >> b[i];
-    
-    // 시작 array를 arr에 표시합니다.
-    for(int i = 1; i <= n; i++) {
-        arr[i] = i;
-        // 각 i 숫자가 위치할 수 있는 자리를 HashSet에 넣어 저장합니다.
-        s[i].insert(i);
-        ans[i] = 1;
+    vector<pair<int, int>> changeSeat(K);
+    for (int i = 0; i < K; ++i) {
+        cin >> changeSeat[i].first >> changeSeat[i].second;
     }
 
-    // 3k번 자리를 바꿉니다.
-    for(int cnt = 0; cnt < 3; cnt++) {
-        for(int i = 0; i < k; i++) {
-            // a[i]와 b[i]의 자리를 바꿔줍니다.
-            // 이때 자리가 변화한 값은 arr[a[i]]와 arr[b[i]]밖에 없습니다.
-            // 이 값들만 새로운 자리를 HashSet에 넣어 저장합니다.
-            swap(arr[a[i]], arr[b[i]]);
+    vector<unordered_set<int>> seats(N + 1);
+    vector<int> current(N + 1);
+    for (int i = 1; i <= N; ++i) {
+        seats[i].insert(i);
+        current[i] = i;
+    }
 
-            if(s[arr[a[i]]].find(a[i]) == s[arr[a[i]]].end()) {
-                s[arr[a[i]]].insert(a[i]);
-                ans[arr[a[i]]]++;
-            }
-            
-            if(s[arr[b[i]]].find(b[i]) == s[arr[b[i]]].end()) {
-                s[arr[b[i]]].insert(b[i]);
-                ans[arr[b[i]]]++;
-            }
+    for (int j = 0; j < 3; ++j) {
+        for (int i = 0; i < K; ++i) {
+            int a = changeSeat[i].first;
+            int b = changeSeat[i].second;
+
+            seats[current[a]].insert(b);
+            seats[current[b]].insert(a);
+
+            swap(current[a], current[b]);
+            /*
+            int temp = current[a];
+            current[a] = current[b];
+            current[b] = temp;
+            */
         }
     }
-    
-    // 3k번 자리를 바꾸면서 각 숫자들이 위치할 수 있는 값들의 개수를 ans 배열에서 확인합니다.
-    for(int i = 1; i <= n; i++)
-        cout << ans[i] << endl;
-    return 0;
+
+    for(int i = 1; i <= N; ++i) {
+        cout << seats[i].size() << '\n';
+    }    
+
+	return 0;
 }
