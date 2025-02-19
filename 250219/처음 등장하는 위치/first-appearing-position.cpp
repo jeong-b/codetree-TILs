61c852090ac4f0ba93833409b58b4888b14b3896
서정빈 <122597075+jeong-b@users.noreply.github.com>
2025-02-19 20:50:58 +0900

    Title: 처음 등장하는 위치, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250219/처음 등장하는 위치/first-appearing-position.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+    
+    int N;
+    map<int, int> m;
+
+    cin >> N;
+
+    for (int i = 0; i < N; ++i) {
+        int input;
+        cin >> input;
+        m.insert({input, i + 1});
+    }
+
+    for (auto it = m.begin(); it != m.end(); ++it) {
+        cout << (*it).first << ' ' << (*it).second << '\n';
+    }
+
+	return 0;
+}
