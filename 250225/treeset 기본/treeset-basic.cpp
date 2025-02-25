900e2b8ae18fcbf7d883f0f6cce4da74bed15d18
서정빈 <122597075+jeong-b@users.noreply.github.com>
2025-02-25 15:09:31 +0900

    Title: treeset 기본, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250225/treeset 기본/treeset-basic.cpp
+++ 250225/treeset 기본/treeset-basic.cpp
@@ -4,11 +4,10 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
     int N;
     cin >> N;
@@ -19,56 +18,34 @@ int main()
         int input;
         string cmd;
         cin >> cmd;
-        
+
         if (cmd == "add") {
             cin >> input;
             numbers.insert(input);
-        }
+        } 
         else if (cmd == "remove") {
             cin >> input;
             numbers.erase(input);
-        }
+        } 
         else if (cmd == "find") {
             cin >> input;
-            if (numbers.find(input) != numbers.end()) {
-                cout << "true\n";
-                continue;
-            }
-            cout << "false\n";
-
-        }
+            cout << (numbers.find(input) != numbers.end() ? "true\n" : "false\n");
+        } 
         else if (cmd == "lower_bound") {
             cin >> input;
-            if (numbers.lower_bound(input) == numbers.end()) {
-                cout << "None\n";
-                continue;
-            }
-            cout << *numbers.lower_bound(input) << '\n';
-        }
+            cout << (numbers.lower_bound(input) == numbers.end() ? "None\n" : to_string(*numbers.lower_bound(input)) + "\n");
+        } 
         else if (cmd == "upper_bound") {
             cin >> input;
-            if (numbers.upper_bound(input) == numbers.end()) {
-                cout << "None\n";
-                continue;
-            }
-            cout << *numbers.upper_bound(input) << '\n';
-
-        }
+            cout << (numbers.upper_bound(input) == numbers.end() ? "None\n" : to_string(*numbers.upper_bound(input)) + "\n");
+        } 
         else if (cmd == "largest") {
-            if (numbers.size() == 0) {
-                cout << "None\n";
-                continue;
-            }
-            cout << *numbers.rbegin() << '\n';
-        }
+            cout << (numbers.empty() ? "None\n" : to_string(*numbers.rbegin()) + "\n");
+        } 
         else if (cmd == "smallest") {
-            if (numbers.size() == 0) {
-                cout << "None\n";
-                continue;
-            }
-            cout << *numbers.begin() << '\n';
+            cout << (numbers.empty() ? "None\n" : to_string(*numbers.begin()) + "\n");
         }
     }
 
-	return 0;
+    return 0;
 }
